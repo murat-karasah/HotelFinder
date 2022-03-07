06e2e983d577d35474596b471dfb2c81f43e1654
Murat Karasah <mkarasah@gmail.com>
2022-03-07 09:18:32 +0300

    v1.1

---
+++ HotelFinderDataAccess/HotelDbContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelFinderEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinderDataAccess
+{
+   public class HotelDbContext:DbContext
+    {
+        public DbSet<Hotel> Holtes{ get; set; }
+    }
+}
